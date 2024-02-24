79994ae3b47faf95e4dc402c9a0d2717abb4bdf0
Caio Henrique <caiogrechic50@gmail.com>
2024-02-23 23:08:20 -0300

    Intodução ao react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,20 +5,16 @@ import {
 } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Carrinho } from './pages/Carrinho'
+import { Erro } from './pages/error/error'
 
 export const Rotas = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='/'
-          element={<Home/>}
-        />
-        <Route
-          path='/carrinho'
-          element={<Carrinho />}
-        />
+        <Route path='/' element={<Home />} />
+        <Route path='/carrinho' element={<Carrinho />} />
+        <Route path='*' element={<Erro />} />
       </Routes>
     </BrowserRouter>
   )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -14,4 +14,4 @@ export const GlobalStyle = createGlobalStyle`
       font-size: 87.75%;
     }
   }
-`
+`;
